a6c51c2f266c5b27d67a9eb234c6065aafafea17
keeezy <lazarokev@gmail.com>
2022-08-04 16:41:37 -0700

    modified tag-routes, able to get all tags and single tag by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,16 +1,37 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [Product],
+    });
+    res.status(200).json(tags);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include:[Product],
+    });
+    if(!singleTag) {
+      return res.status(500).json({
+        message: `This ID doesn't exist. Please enter a valid ID!`
+      })
+    }
+    res.status(200).json(singleTag);
+  } catch(err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
