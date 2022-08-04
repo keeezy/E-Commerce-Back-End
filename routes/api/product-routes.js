eff38e0be27a0e5ea5831d005c496cd6c4fabab8
keeezy <lazarokev@gmail.com>
2022-08-04 15:55:13 -0700

    modified product-routes, able to get all products, and single product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+// GET api/products
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [Category, Tag]
+    });
+    res.status(200).json(products);
+  } catch(err) {
+    return res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProduct = await Product.findByPk(req.params.id, {
+      include: [Category, Tag,]
+    });
+    if (!singleProduct) {
+      return res.status(500).json({
+        message: `This product ID doesn't exist. Please enter a valid product ID!`
+      });
+    }
+    res.status(200).json(singleProduct);
+  } catch (err) {
+    return res.status(500).json(err)
+  }
 });
 
 // create new product
