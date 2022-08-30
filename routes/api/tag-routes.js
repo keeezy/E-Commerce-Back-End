63a95f31333227a6ca5429a04de4c4c2721bf5d0
keeezy <lazarokev@gmail.com>
2022-08-30 14:27:42 -0700

    modified tag-routes, able to create,edit, and delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,7 @@ const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET /api/tags
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
@@ -16,6 +17,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET /api/tags/:id
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -34,16 +36,53 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+// POST /api/tags
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const createTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(createTag);
+  } catch (err) {
+    return res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+// PUT /api/tags/:id
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const checkTagId = await Tag.findByPk(req.params.id);
+    const updateTag = await Tag.update (
+      {tag_name: req.body.tag_name},
+      {where: {id:req.params.id}}
+    );
+    if(!checkTagId) {
+      return res.status(404),json({message: `This tag ID doesn't exist. Please enter a valid ID!`})
+    }
+    res.status(200).json(err);
+  } catch(err) {
+    return res.status(500).json;
+  }
 });
 
-router.delete('/:id', (req, res) => {
+// DELETE /api/tags/:id
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const checkTagId = await Tag.findByPk(req.params.id);
+    const deleteTag = await Tag.destroy(
+      {where: {id: req.params.id}}
+    )
+    if(!deleteTag) {
+      res.status(404).json ({message: 'No tag found with this ID'})
+      return
+    }
+    res.status(200).json({message: 'Tag Deleted'})
+  } catch (err) {
+    res.status(500).json('Something went wrong', err)
+  }
 });
 
 module.exports = router;
